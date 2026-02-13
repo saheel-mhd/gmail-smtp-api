7017160b9243ce8e61b3a6ff5e6e12a4a75fa7ed
muhammed saheel <137157119+saheel-mhd@users.noreply.github.com>
2026-02-13 16:33:31 +0400

    fix prod

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -20,5 +20,5 @@ export default function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
+  matcher: ["/((?!api|admin|v1|_next/static|_next/image|favicon.ico).*)"]
 };
